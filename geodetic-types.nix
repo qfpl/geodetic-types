fb567b265369db5087cc18af4563a246d902ca91
George Wilson <george@wils.online>
2018-07-13 15:49:39 +1000

    Fix build on GHC 7.10

--- geodetic-types.nix
+++ geodetic-types.nix
@@ -1,9 +1,9 @@
-{ mkDerivation, base, dimensional, lens, stdenv }:
+{ mkDerivation, base, dimensional, lens, semigroups, stdenv }:
 mkDerivation {
   pname = "geodetic-types";
   version = "0.0.1";
   src = ./.;
-  libraryHaskellDepends = [ base dimensional lens ];
+  libraryHaskellDepends = [ base dimensional lens semigroups ];
   homepage = "https://github.com/qfpl/geodetic-types";
   description = "Types for geodetic operations";
   license = stdenv.lib.licenses.bsd3;
